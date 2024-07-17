3d9fe2bcc2fe4902b117296bf972a06b4590b379
Amit Kumar <amian.nitan.2000@gmail.com>
2024-07-17 15:50:04 +0530

    completed the basic understanding.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,10 +1,12 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -24,5 +26,38 @@ public class StudentService {
 //        return List.of(s1,s2);
 
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student){
+
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+//        System.out.println("student - already present in database." );
+        if(!studentByEmail.isPresent()){
+            studentRepository.save(student);
+        } else {
+            System.out.println("student - "+ student.getName() +" already present in database." );
+        }
+    }
+
+    public void deleteStudent(Long id){
+        if(studentRepository.existsById(id)){
+            studentRepository.deleteById(id);
+        } else {
+            System.out.println("Student Is Not Present In The Database.");
+        }
+    }
+
+    @Transactional
+    public void updateStudent(Long id, String name, String email){
+        Student studentById = studentRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("Studnet with id " + id + "does not exist"));
+
+        if(email != null && !email.isEmpty()){
+            studentById.setEmail(email);
+        }
+
+        if(name != null && !name.isEmpty()){
+            studentById.setName(name);
+        }
     }
 }
